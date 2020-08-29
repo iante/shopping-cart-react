4b5ba19d29de1181809c46d191a036de6674d22f
iante <maxmaine66@gmail.com>
2020-08-29 18:29:30 +0300

    first commit

---
+++ store/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+function App() {
+  return (
+  <div className="grid-container">
+    <header>
+      <a href="/">React Shopping Cart</a>
+    </header>
+    <main>
+      Product List
+    </main>
+    <footer>
+      Created By Ian Murithi
+    </footer>
+  </div>
+  );
+}
+
+export default App;
+
+//git remote add origin git@github.com:iante/react-shopping-cart.git
+//git push -u origin master
