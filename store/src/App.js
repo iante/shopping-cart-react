030310bfd982fafb28ec305335a25a52962a906a
iante <maxmaine66@gmail.com>
2020-09-01 18:10:36 +0300

    add redux to cart

--- store/src/App.js
+++ store/src/App.js
@@ -8,54 +8,7 @@ import { Provider } from "react-redux";
 
 class App extends React.Component //converting a functional component to a class component 
 {
-  constructor(){
-    super();
-    this.state = { //setting initial state
-      //products: data.products,
-      //JSON.parse converts string to json set in localStorage
-      cartItems:localStorage.getItem("cartItems")? 
-      JSON.parse(localStorage.getItem("cartItems")): [],
-     
-    };
-  }
-createOrder = (order) => {
-  alert("Need to Save Order for" + order.name)
-}
-  removeFromCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    this.setState({
-//removes current product user selected to remove
-cartItems: cartItems.filter((x=>x._id !== product._id)),
-    });
-
-    localStorage.setItem("cartItems", JSON.stringify( cartItems.filter((x)=>x._id !== product._id))
-    );
-  };
-
-  addToCart = (product) => {
-const cartItems = this.state.cartItems.slice();
-let alreadyInCart = false;
-
-//checks each item added in the cart and if item id is equal to product id, it increments the cart by 1
-cartItems.forEach(item => {
-  if(item._id === product._id){
-    item.count++;
-    alreadyInCart = true;
-  }
-});
-//if item wasnt already in cart, spread it and add it to the array
-if(!alreadyInCart){
-  cartItems.push({...product, count: 1})
-}
-
-this.setState({cartItems});
-
-//saving the cart items when the page is refreshed
-localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  };
-
   
-
   render(){
     return (
       <Provider store={store}>
@@ -69,12 +22,11 @@ localStorage.setItem("cartItems", JSON.stringify(cartItems));
             {/*this.state.products.length displays the total number of products as a result */}
             <Filter/>
             {/*passing this.state to Products component as a prop  */}
-            <Products products={this.state.products}/>
+            <Products/>
           </div>
           <div className="sidebar">
-             <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart}
-             createOrder={this.createOrder}
-             /></div>
+             <Cart/>
+             </div>
         </div>
         </main>
         <footer>
